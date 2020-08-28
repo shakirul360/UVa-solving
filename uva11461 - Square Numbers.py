42e862a5453208eeac22ff594ad47763d2f37497
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-08-29 00:16:59 +0600

    Update uva11461 - Square Numbers.py
    Fixed code. Usage of ceil() and floor method of math module to lower the execution time.

--- uva11461 - Square Numbers.py
+++ uva11461 - Square Numbers.py
@@ -1,13 +1,9 @@
-import math
+from math import floor, ceil, sqrt
 
 while True:
     a, b = map(int,input().split())
     total = 0
     if a == 0 and b == 0:
         break
-    for i in range(a,b + 1):
-        root = math.sqrt(i)
-        if root == int(root):
-            total += 1
-            #print(root)
-    print(total)
+    res = floor(sqrt(b)) - ceil(sqrt(a)) + 1
+	print(res)
