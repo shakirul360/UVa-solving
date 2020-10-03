bb6a525e39ce6192937d9fe25bdaf7b3efe834b4
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-10-04 03:35:34 +0600

    Update uva10260 - Soundex (Solved)
    The code prints out the Soundex code of the string provided. Use of remove_adjacent method was used to remove same adjacent items in the array.

--- 10260 - Soundex(not complete).py
+++ 10260 - Soundex(not complete).py
@@ -2,25 +2,27 @@ catalog = {"B" : 1, "F": 1, "P": 1, "V" : 1,
            "C" : 2, "G" : 2, "J" : 2, "K" : 2, "Q" : 2, "S" : 2, "X": 2, "Z" : 2,
            "D" : 3, "T" : 3, "L" : 4, "M" : 5 , "N" :5, "R" : 6}
 
-
-while True:
-    res_string = ''
-    string = []
-    inp = input()
-    for _ in inp:
-        string.append(_)
-    try:
-        for i in range(len(string)):
-            x = string[i]
-    #y = string[i + 1]
-            if x in catalog:
-                res_string += str(catalog[x])
-                if y in catalog:
-                    try:
-                        string.pop(i + 1)
-                    except:
-                        pass
-    except:
-        pass
+def remove_adjacent(nums):
+     return [a for a,b in zip(nums, nums[1:]+[not nums[-1]]) if a != b]
     
-        print(res_string)
+while True:
+    arr = []
+    try:
+        string = input()
+    except EOFError:
+        break
+        
+    for i in string:
+        if i in catalog:
+            arr.append(catalog[i])
+        else:
+            arr.append(0)
+            
+    arr = remove_adjacent(arr)
+    
+    res_string = ''
+    for i in arr:
+        if i > 0:
+            res_string += str(i)
+            
+    print(res_string)
