314206f7d8996944f93a889da7f71412adbf8a7d
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-10-04 17:14:06 +0600

    uva 10006 - Carmichael Numbers (Solved)

--- uva10006Carmichael_Numbers.py
+++ uva10006Carmichael_Numbers.py
@@ -1,35 +1,10 @@
-import random 
-
-def sieve(n):
-    prime = [True for i in range(n + 1)]
-    p = 2
-    while (p * p <= n):
-        if prime[p] == True:
-            for i in range(p * p, n + 1, p):
-                prime[i]= False
-        p += 1
-        arr = []
-    for i in range(2,n + 1):
-        if prime[i] == True:
-            arr.append(i)
-    return arr
-
-def fermat(n):
-    for i in range(5):
-        a = random.randint(2, n - 1)
-        if (pow(a, n) % n ) == a:
-            result = True
-        else:
-            result = False
-            return result
-    return result
+carmichael = [561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341, 41041, 46657, 52633, 62745,  63973]
 
 while True:
     n = int(input())
     if n == 0:
         break
-    arr = sieve(n)
-    if fermat(n) == True and  n in arr[::-1]:
-        print("{} is normal.".format(n))
-    else:
+    if n in carmichael:
         print("The number {} is a Carmichael number.".format(n))
+    else:
+        print("{} is normal.".format(n))
