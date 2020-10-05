62626b221669d80643980456c4ff8f9fa483ff6c
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-10-05 21:46:34 +0600

    uva713 - Adding Reversed Numbers (Solved)
    Reference and help was taken from StackOverflow. link - https://stackoverflow.com/questions/36070152/adding-reversed-numbers-in-python

--- uva713Adding_Reversed_Numbers.py
+++ uva713Adding_Reversed_Numbers.py
@@ -1,17 +1,9 @@
 n = int(input())
 for i in range(n):
     a, b = input().split()
-    len_diff = abs(len(a) - len(b))
-    if len(b) > len(a):
-        a, b = b, a
-        b = b.ljust(len_diff + len(b), '0')
-    else:
-        b = b.ljust(len_diff + len(b), '0')
-    print(a,b)
-    a = a[::-1]
-    b = b[::-1]
-    print(a,b)
-    res = ''
-    for num in range(len(a)):
-        res += str(int(a[num]) + int(b[num]))
-    print(res)
+    a = int((a)[::-1])
+    b = int((b)[::-1])
+    res = a + b
+    res_rev = str(res)[::-1]
+    #print(a,b)
+    print(int(res_rev))
