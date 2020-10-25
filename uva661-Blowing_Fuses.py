ee5b765f2e3114175e5817caf3bb660972912900
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-10-26 02:10:57 +0600

    uva661 - Blowing Fuses (FIXED)
    the code was breaking off the input whenever the current used was greater than capacity. Accordingly fixed.

--- uva661-Blowing_Fuses.py
+++ uva661-Blowing_Fuses.py
@@ -1,32 +1,31 @@
 tcase = 1
 while True:
-    devices, operations, capacity = map(int,input().split())
+    devices, operations, capacity = map(int, input().split())
     if devices == 0 and operations == 0 and capacity == 0:
         break
     devs = [0]
-    dev_bool = [False for i in range(devices + 1)]
+    devs_bool = [False for d in range(devices + 1)]
+    highest = 0
+    top = 0
     blown = False
-    for device in range(devices):
+    for i in range(devices):
         dv = int(input())
         devs.append(dv)
-    top = 0
-    highest = 0
-    for ope in range(operations):
-        #print(highest)
-        operation  = int(input())
-        if dev_bool[operation] == False:
+    for op in range(operations):
+        operation = int(input())
+        if devs_bool[operation] == False:
             highest += devs[operation]
-            dev_bool[operation] = True
+            devs_bool[operation] = True
             if top < highest:
                 top = highest
         else:
-            dev_bool[operation] = False
             highest -= devs[operation]
+            devs_bool[operation] = False
         if highest > capacity:
             blown = True
-            break
+        
     print("Sequence {}".format(tcase))
-    if blown == True:
+    if blown:
         print("Fuse was blown.")
     else:
         print("Fuse was not blown.")
