6e414927dc08cf10eae5ef1187a72b1f57849f22
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-11-03 05:02:50 +0600

    Update uva12503-Robot_Instructions.py

--- uva12503-Robot_Instructions.py
+++ uva12503-Robot_Instructions.py
@@ -1,24 +1,29 @@
-def move(comms, index):
-    #global pointer
-    comm = comms[index]
-    if comm == "LEFT":
-        pointer -= 1
-    elif comm == "RIGHT":
-        pointer += 1
-    else:
-        new_idx = int(comm[-1]) - 1
-        return move(comms,new_idx)
+import sys
+cmd_list,x = [],0
 
-pointer = 0
-tcase = int(input())
-for case in range(tcase):
-    #pointer = 0
-    steps = int(input())
-    comms = []
-    for step in range(steps):
-        comm = input()
-        comms.append(comm)
-    for comm in range(steps):
-        move(comms, comm)
-    print(pointer)
-    pointer = 0
+def same_as(c):
+    if not (cmd_list[c][0] == "SAME AS"):
+        global x
+        x+=cmd_list[c][1]
+    else:
+        same_as(cmd_list[c][1])
+
+tc = int(sys.stdin.readline())
+for t in range(tc):
+    n = int(sys.stdin.readline())
+    x = 0
+    for i in range(n):
+        cmd =  input()      
+        if(cmd == "LEFT"):
+            cmd_list.append(["LEFT",-1])
+            x+=(-1)
+        elif(cmd == "RIGHT"):
+            cmd_list.append(["RIGHT",1])
+            x+=1
+        else:
+            a,b,c = map(str,cmd.split())
+            c = int(c)-1
+            cmd_list.append(["SAME AS",c])
+            same_as(c)
+    print(x)
+    cmd_list.clear()
