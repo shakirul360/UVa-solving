617e63e715028efbde2361ad38e1b4727c2be958
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-11-06 22:07:24 +0600

    Update uva10487-Closest_Sums.py

--- uva10487-Closest_Sums.py
+++ uva10487-Closest_Sums.py
@@ -1,23 +1,33 @@
-case = 1
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    arr = []
-    for i in range(n):
-        arr.append(int(input()))
-    print("Case {}:".format(case))
-    qn = int(input())
-    for q in range(qn):
-        q = int(input())
-        closest = arr[0] + arr[1]
-        ab = abs(q - closest)
-        for i in range(len(arr)):
-            for j in arr[i + 1:]:
-                s = arr[i] + j
-                abx = abs(q - s)
-                if abx < ab:
-                    ab = abx
-                    closest = s
-        print("Closest sum to {} is {}.".format(q,closest))
-    case += 1
+import sys
+import itertools
+import bisect
+
+def fn(z): return sum(z)
+
+w = 0
+for s in sys.stdin:
+    n = int(s)
+    if(n == 0): break
+    w += 1
+    f = []
+    for _ in range(n):
+        f.append(int(sys.stdin.readline()))
+    t = sorted(list(map(fn, itertools.combinations(f, 2))))
+    m = int(sys.stdin.readline())
+    print('Case {}:'.format(w))
+    #print(t)
+    gh = len(t)
+    for _ in range(m):
+        h = int(sys.stdin.readline())
+        v = bisect.bisect_left(t, h)
+        r = 0
+        #print(v)
+        if(v == gh): r = t[-1]
+        elif(v == 0): r = t[0]
+        else:
+            r1, r2 = t[v], t[v-1]
+            if(abs(r1-h) > abs(r2-h)):
+                r = r2
+            else:
+                r = r1
+        print('Closest sum to {} is {}.'.format(h, r))
