4e3ada009fcd604b2085c05c43eeaf0060b606f2
Shakirul Islam Leeon <shakirul360@gmail.com>
2021-02-11 23:13:04 +0600

    uva10038

---
+++ C++/uva10038.cpp
@@ -0,0 +1,29 @@
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+ 
+using namespace std;
+ 
+int n;
+int ar[3100], dar[3100];
+ 
+bool jolly() {
+    for(int i = 0; i < n - 1; i++)
+        dar[i] = abs(ar[i + 1] - ar[i]);
+    sort(dar, dar + n - 1);
+    for(int i = 0; i < n - 1; i++)
+        if(dar[i] != i + 1) {
+            printf("Not jolly\n");
+            return false;
+        }
+    printf("Jolly\n");
+    return true;
+}
+ 
+int main() {
+    while(scanf("%d", &n) == 1) {
+        for(int i = 0; i < n; i++)
+            scanf("%d", &ar[i]);
+        jolly();
+    }
+}
