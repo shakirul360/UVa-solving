1416a0faebbcd3d49122257ed72e1c7528a59442
Shakirul Islam Leeon <shakirul360@gmail.com>
2021-09-25 14:10:19 +0600

    day 3

---
+++ Vjudge x NSU/Isthistheeasiestproblem.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int tcase, a, b, c, i;
+
+    scanf("%d", &tcase);
+
+    for (i = 0; i < tcase; i++){
+        scanf("%d %d %d", &a, &b, &c);
+
+        if (a + b < c || b + c < a || c + a < b){
+            printf("Case %d: Invalid\n", i + 1);
+        } else if (a == b && b == c){
+            printf("Case %d: Equilateral\n", i + 1);
+        } else if (a == b || b == c || a == c){
+            printf("Case %d: Isosceles\n", i + 1);
+        } else {
+            printf("Case %d: Scalene\n", i + 1);
+        }
+    }
+
+    return 0;
+}
---
+++ Vjudge x NSU/ToyArmy.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+    int n, res;
+
+    scanf("%d", &n);
+
+    printf("%d\n", 3*n / 2);
+
+
+    return 0;
+}
